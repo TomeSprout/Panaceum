09d6bca18ef0240742e6a608a588cb28891e2800
Timothy <TomeSprout@pm.me>
2022-10-20 19:38:43 -0400

    Add Notion get request boilerplate

--- server/src/middleware/OrganizeMultiSelect.ts
+++ server/src/middleware/OrganizeMultiSelect.ts
@@ -1,4 +1,17 @@
-import { Client } from "@notionhq/client";
+import { Client } from "@notionhq/client"
+import dotenv from "dotenv"
 
-const notion = new Client({ auth: process.env.NOTION_TOKEN });
-const databaseId: string = process.env.NOTION_DATABASE_ID as string;
+dotenv.config()
+
+const OrganizeMultiSelects = async () => {
+  const notion = new Client({ auth: process.env.NOTION_TOKEN })
+  const databaseId: string = process.env.NOTION_DATABASE_ID as string
+  
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  })
+
+  console.log(response)
+}
+
+export default OrganizeMultiSelects
