b0631b00a11c289cdb5a036b949aecd0b968883a
Timothy <TomeSprout@pm.me>
2022-10-24 22:07:59 -0400

    GET Database Properties
    Change query to retrieve method to access DB properties
    Set type to explicitly define property values

--- server/src/middleware/OrganizeMultiSelect.ts
+++ server/src/middleware/OrganizeMultiSelect.ts
@@ -9,7 +9,7 @@ const OrganizeMultiSelects = async () => {
   const notion = new Client({ auth: process.env.NOTION_TOKEN })
   const databaseId: string = process.env.NOTION_DATABASE_ID as string
   
-  const { object } = await notion.databases.query({
+  const { properties } = await notion.databases.retrieve({
     "database_id": databaseId,
   })
 
@@ -22,11 +22,23 @@ const OrganizeMultiSelects = async () => {
   //   }
   // }
 
-  const stringifyResponse = JSON.stringify(object)
-  fs.writeFile('res.json', stringifyResponse, (err: Error) => {
-    if (err) throw err;
-    console.log("new data added to file")
-  })
+  // const stringifyResponse = JSON.stringify(properties)
+  // fs.writeFile('res.json', stringifyResponse, (err: Error) => {
+  //   if (err) throw err;
+  //   console.log("new data added to file")
+  // })
+
+  type MultiSelectDatabasePropertyConfigResponse = {
+    type: "multi_select";
+    multi_select: {
+        options: Array<Object>;
+    };
+    id: string;
+    name: string;
+  };
+
+  const opt: MultiSelectDatabasePropertyConfigResponse = properties.Genre
+  console.log(opt)
 
   // const {multiSelect} = response.results[0].properties.Genre.multi_select
 }
