9fd80c96f23298a2d1eea59a072f9bc47e9d05d1
Timothy <TomeSprout@pm.me>
2022-10-24 21:43:35 -0400

    Add JSON stringify & file write for Response JSON

--- server/src/middleware/OrganizeMultiSelect.ts
+++ server/src/middleware/OrganizeMultiSelect.ts
@@ -1,17 +1,34 @@
 import { Client } from "@notionhq/client"
 import dotenv from "dotenv"
 
+const fs = require('fs');
+
 dotenv.config()
 
 const OrganizeMultiSelects = async () => {
   const notion = new Client({ auth: process.env.NOTION_TOKEN })
   const databaseId: string = process.env.NOTION_DATABASE_ID as string
   
-  const response = await notion.databases.query({
-    database_id: databaseId,
+  const { object } = await notion.databases.query({
+    "database_id": databaseId,
   })
 
-  console.log(response)
+  // Filter for DB Query
+  //
+  // "filter": {
+  //   "property": "Genre",
+  //   "multi_select": {
+  //     "is_not_empty": true
+  //   }
+  // }
+
+  const stringifyResponse = JSON.stringify(object)
+  fs.writeFile('res.json', stringifyResponse, (err: Error) => {
+    if (err) throw err;
+    console.log("new data added to file")
+  })
+
+  // const {multiSelect} = response.results[0].properties.Genre.multi_select
 }
 
 export default OrganizeMultiSelects
