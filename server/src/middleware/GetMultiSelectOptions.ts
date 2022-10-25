e6c0adcc090ae477d1e499cbc67eae4c9657dd99
Timothy <TomeSprout@pm.me>
2022-10-25 09:20:41 -0400

    Move logic for receiving properties to new file

---
+++ server/src/middleware/GetMultiSelectOptions.ts
@@ -0,0 +1,47 @@
+import { Client } from '@notionhq/client'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const OrganizeMultiSelects = async () => {
+  const notion = new Client({ auth: process.env.NOTION_TOKEN })
+  const databaseId: string = process.env.NOTION_DATABASE_ID as string
+
+  const { properties }: { properties: any } = await notion.databases.retrieve({
+    database_id: databaseId,
+  })
+
+  type StringRequest = string
+  type SelectColor =
+    | 'default'
+    | 'gray'
+    | 'brown'
+    | 'orange'
+    | 'yellow'
+    | 'green'
+    | 'blue'
+    | 'purple'
+    | 'pink'
+    | 'red'
+  type SelectPropertyResponse = {
+    id: StringRequest
+    name: StringRequest
+    color?: SelectColor
+  }
+
+  interface MultiSelectDatabasePropertyOptions {
+    options: Array<SelectPropertyResponse>
+    forEach(
+      callback: (propertyOptionObject: SelectPropertyResponse) => void
+    ): void
+  }
+
+  const opt: MultiSelectDatabasePropertyOptions =
+    properties.Genre.multi_select.options
+  opt.forEach((element) => {
+    delete element.color
+  })
+  console.log(`Opt:`, opt)
+}
+
+export default OrganizeMultiSelects
