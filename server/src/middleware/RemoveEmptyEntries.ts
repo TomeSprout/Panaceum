df9fa0ef169da3d529907d72998876a51ef00238
Timothy <TomeSprout@pm.me>
2022-11-03 08:34:55 -0400

    Create boilerplate for RemoveEmptyEntries.ts
    Uses Notion API to filter out entries where title is empty

---
+++ server/src/middleware/RemoveEmptyEntries.ts
@@ -0,0 +1,24 @@
+import { Client } from '@notionhq/client'
+import { config } from 'dotenv'
+import WriteToJSON from './WriteToJSON'
+
+config()
+
+const RemoveEmptyEntries = async () => {
+  const notion = new Client({ auth: process.env.NOTION_TOKEN })
+  const databaseId: string = process.env.NOTION_DATABASE_ID as string
+
+  const { results }: { results: any } = await notion.databases.query({
+    database_id: databaseId,
+    filter: {
+      property: "title",
+      title: {
+        is_empty: true
+      }
+    },
+  })
+
+  WriteToJSON(results)
+}
+
+export default RemoveEmptyEntries
