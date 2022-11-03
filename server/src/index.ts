b7323266a7f1581dc7fc3d4212ec05491b4a8f2c
Timothy <TomeSprout@pm.me>
2022-11-03 08:39:40 -0400

    Call RemoveEmptyEntries fn in index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import OrganizeMultiSelects from './middleware/OrganizeMultiSelects'
+import RemoveEmptyEntries from './middleware/RemoveEmptyEntries'
 
 const main = async () => {
   // await OrganizeMultiSelects()
-  
+  await RemoveEmptyEntries()
 }
 
 main()
