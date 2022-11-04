f1e00333e264bbe32c6d552a89e9c9596105da9a
Timothy <TomeSprout@pm.me>
2022-11-04 17:05:28 -0400

    Add variables for separate regexp checks
    Separate by Latin Alphabet, Numerical Digits, Common Keyboard Symbols
    (not escaped in RegExp), Common Keyboard Symbols (escaped in RegExp),
    Mac Keyboard Symbols, CJK Ideographs, Japanese Hiragana and Katakana,
    and Korean Hangul

--- server/src/middleware/RemoveEmptyEntries.ts
+++ server/src/middleware/RemoveEmptyEntries.ts
@@ -4,6 +4,15 @@ import { config } from 'dotenv'
 config()
 
 const isNotEmpty = (str: string | undefined) => {
+  const checkLatinAlphabetCharacters = /[a-zA-Z]/
+  const checkNumericDigits = /[0-9]/
+  const checkCommonKeyboardSymbols = /[!@#%&{};:'",.<>?~`_=]/
+  const checkCommonKeyboardSymbolsEscaped = /[\\\/\.\[\]\^\$\+\-\*\?\|\(\)]/
+  const checkMacKeyboardSymbols = /[⌘⌥⌃⇧⇪]/
+  const checkCjkIdeographs = /[\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff]/
+  const checkJapaneseHiraganaKatakana = /[\u3040-\u30ff\uff66-\uff9f]/
+  const checkKoreanHangul = /[\u3131-\uD79D]/
+
   if (str !== undefined) {
     return /[a-zA-Z0-9\\\/\.\[\]\^\$\+\-\*\?\|\(\)!@#%&{};:'",.<>?~`_=\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f\u3131-\uD79D]/.test(str)
   }
