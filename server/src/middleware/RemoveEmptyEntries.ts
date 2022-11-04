d86551712b2e9045246827f0f57541f49195c795
Timothy <TomeSprout@pm.me>
2022-11-04 12:34:43 -0400

    Send PATCH request and archive(delete) empty entry

--- server/src/middleware/RemoveEmptyEntries.ts
+++ server/src/middleware/RemoveEmptyEntries.ts
@@ -22,18 +22,22 @@ const RemoveEmptyEntries = async () => {
 
   results.forEach((element: any) => {
     const properties = Object.entries(element.properties)
-    properties.forEach((property: any) => {
+    properties.forEach(async (property: any) => {
       if (property[1].id === 'title' || property[1].type === 'title') {
         if (
           property[1].title.length === 0 ||
           !containsAlphanumerics(property[1].title[0].plain_text)
         ) {
           emptyEntries.push(element)
+
+          await notion.pages.update({
+            page_id: element.id,
+            archived: true,
+          })
         }
       }
     })
   })
-
 }
 
 export default RemoveEmptyEntries
