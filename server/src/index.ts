22f5e75474d6805440839652c7a31251f4ef6e3c
Timothy <TomeSprout@pm.me>
2022-11-05 00:02:56 -0400

    Make call to NotionPageIconCover fn

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import OrganizeMultiSelects from './middleware/OrganizeMultiSelects'
 import RemoveEmptyEntries from './middleware/RemoveEmptyEntries'
+import NotionPageIconCover from './middleware/NotionPageIconCover'
 
 const main = async () => {
   // await OrganizeMultiSelects()
-  await RemoveEmptyEntries()
+  // await RemoveEmptyEntries()
+  await NotionPageIconCover()
 }
 
 main()
