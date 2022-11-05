f23698632eb67f30ce5656e0bb1cf45cf47f0161
Timothy <TomeSprout@pm.me>
2022-11-05 00:01:40 -0400

    Add boilerplate for fn

---
+++ server/src/middleware/NotionPageIconCover.ts
@@ -0,0 +1,15 @@
+import { Client } from '@notionhq/client'
+import { config } from 'dotenv'
+
+config()
+
+const CheckIconCover = async () => {
+  const notion = new Client({ auth: process.env.NOTION_TOKEN })
+  const databaseId: string = process.env.NOTION_DATABASE_ID as string
+
+  const { results }: { results: any } = await notion.databases.query({
+    database_id: databaseId,
+  })
+}
+
+export default CheckIconCover
