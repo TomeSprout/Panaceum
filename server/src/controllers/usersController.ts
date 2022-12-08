b658382d5d9e138e8ae31a71ed57db0c24fe0efe
Timothy <TomeSprout@pm.me>
2022-12-08 13:45:27 -0500

    Add logic for creating new user
    Create user object with username, password, and email
    Email address is exclusive

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
 import User from '../models/User'
 
 interface notionKey {
@@ -12,11 +13,42 @@ interface todoistKey {
 // @desc Create new Client Key
 // @route POST /key
 // @access Private
-export const createKey = async (req: Request, res: Response): Promise<void> => {
-  const key: notionKey | todoistKey = req.body
+export const createNewUser = async (req: Request, res: Response): Promise<void> => {
+  const { email, username, password, } = req.body
 
-  if (typeof key !== 'string') {
-    res.status(400).json({ message: `Invalid key` })
-    return
+    // Confirm data
+    if (!email || !username || !password) {
+      res.status(400).json({ message: 'All fields are required' })
+      return
+    }
+
+    // Check for duplicates
+    const duplicate = await User.findOne({ email }).lean().exec()
+
+    if (duplicate) {
+      res.status(409).json({ message: 'Duplicate user' })
+      return
+    }
+
+    // Hash password
+    const hashedPassword: string = await bcrypt.hash(password, 10) // salt rounds
+
+    const userObject: {
+      email: string
+      username: string
+      password: string
+    } = {
+      email,
+      username,
+      password: hashedPassword,
+    }
+
+    // Create and store new user
+    const user = await User.create(userObject)
+
+    if (user) {
+      res.status(201).json({ message: `New user ${username} created` })
+    } else {
+      res.status(400).json({ message: 'Invalid user data received' })
+    }
   }
-}
