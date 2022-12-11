8a56b7f14e720b9af57d14fe520015727872f688
Timothy <TomeSprout@pm.me>
2022-12-11 13:11:45 -0500

    Import dotenv, express, cors, path
    Set up server app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,18 @@
+import * as dotenv from 'dotenv'
+import * as express from 'express'
+import * as cors from 'cors'
+import * as path from 'path'
+
+import { connection as MongoDBConnection } from 'mongoose'
+
 import OrganizeMultiSelects from './middleware/OrganizeMultiSelects'
 import RemoveEmptyEntries from './middleware/RemoveEmptyEntries'
 import NotionPageIconCover from './middleware/NotionPageIconCover'
+
+dotenv.config()
+
+const app = express.default()
+const PORT: string | number = (process.env.PORT as string) || 3500
 
 const main = async () => {
   // await OrganizeMultiSelects()
