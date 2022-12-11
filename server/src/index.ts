0d4948a8e1832b623f3d615f92f21db2bcd4fca1
Timothy <TomeSprout@pm.me>
2022-12-11 17:54:22 -0500

    Add paths to routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,6 +21,20 @@ databaseConnection()
 
 app.use(cors.default(corsOptions))
 app.use(express.json())
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', require('./routes/root'))
+app.use('/users', require('./routes/userRoutes'))
+app.use('*', (req, res) => {
+  res.status(404)
+
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' })
+  } else {
+    res.type('txt').send('404 Not Found')
+  }
+})
 
 const main = async () => {
   // await OrganizeMultiSelects()
