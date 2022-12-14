ff12c9b0977b333ffdaa0ce17e2031c0f92e086b
Timothy <TomeSprout@pm.me>
2022-12-14 13:42:17 -0500

    Refactor updateNotionPages for async/await
    Return Promise of void
    Add properties key to update call for sorting MultiSelect Options

--- server/src/services/notion/updateNotionPages.ts
+++ server/src/services/notion/updateNotionPages.ts
@@ -6,9 +6,10 @@ dotenv.config()
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN })
 
-const updateNotionPages = (params: UpdatePageParameters): void => {
-  notion.pages.update({
+const updateNotionPages = async (params: UpdatePageParameters): Promise<void> => {
+  await notion.pages.update({
     page_id: params.page_id,
+    properties: params.properties,
     icon: params.icon,
     cover: params.cover,
     archived: params.archived,
