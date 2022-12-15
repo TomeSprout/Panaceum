b4aa08e30f7e15992ca7c85b7e84a2c7467cca61
Timothy <TomeSprout@pm.me>
2022-12-15 09:51:49 -0500

    Add basic login component generated via Mantine

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { MantineProvider, Button, Stack } from "@mantine/core";
 import { theme } from "./theme";
 import { FeaturesGrid, MOCKDATA } from "./components/FeatureExample";
+import AuthenticationTitle from "./components/Login";
 import APISetup from "./components/APISetup";
 
 const App = () => {
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
       <Stack align="center" mt={50}>
+        <AuthenticationTitle />
         <APISetup title={"Panaceum"} description={""} />
         <FeaturesGrid title={"Panaceum"} description={""} data={MOCKDATA}/>
       </Stack>
