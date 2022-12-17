1afdf3beffe2c547f831046a4660f25e784416e0
Timothy <TomeSprout@pm.me>
2022-12-16 20:31:04 -0500

    Update layout with ColorSchemeProvider
    Serves color scheme toggle functionality
    Default color scheme is dark
    Renders ColorSchemeToggle

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,8 +1,35 @@
-import { Outlet } from "react-router-dom"
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  Container,
+  MantineProvider,
+} from '@mantine/core'
+import { ColorSchemeToggle } from './ColorSchemeToggle'
 
 const Layout = () => {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark')
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+  }
+
   return (
-    <Outlet/>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Container size="md" my={40}>
+          <ColorSchemeToggle />
+        </Container>
+        <Outlet />
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
 
