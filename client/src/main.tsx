fc4826c467a0863fb5f018e665bedc721ed5ba58
Timothy <TomeSprout@pm.me>
2022-12-16 20:25:05 -0500

    Modify main render with React Router DOM
    Uses BrowserRouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,14 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
