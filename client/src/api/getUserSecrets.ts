3e5264992e21c6db18ae8970f5151fdd4e28ff5e
Timothy <TomeSprout@pm.me>
2022-12-17 23:16:41 -0500

    Add fn to getUserSecrets from endpoint

---
+++ client/src/api/getUserSecrets.ts
@@ -0,0 +1,11 @@
+import { API_URL } from "./config"
+
+export const getUserSecrets = async () => {
+  const response = await fetch(`${API_URL}/secrets`, { credentials: 'include' })
+
+  if (response.status === 401) {
+    throw new Error('Authentication Failure')
+  } else {
+    return response.json()
+  }
+}
