a17ff4519d7ae615fb54e7f62df59ca3f270f941
Timothy <TomeSprout@pm.me>
2022-12-18 11:33:36 -0500

    Add logo component that links to home

---
+++ client/src/components/PanaceumLogo.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+import { ActionIcon, Tooltip } from '@mantine/core'
+import logo from '../../public/potion-placeholder-svgrepo-com.svg'
+
+const PanaceumLogo = () => {
+  return (
+    <Link to="/">
+      <Tooltip label="Panaceum" position="bottom">
+        <ActionIcon size="xl" radius='xl'>
+          <img src={logo} alt="Panaceum logo" />
+        </ActionIcon>
+      </Tooltip>
+    </Link>
+  )
+}
+
+export default PanaceumLogo
