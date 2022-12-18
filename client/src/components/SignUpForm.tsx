53c8f139ac19923fe121785788f5aab66f93bf3f
Timothy <TomeSprout@pm.me>
2022-12-18 08:51:16 -0500

    Add SignUp Form component

---
+++ client/src/components/SignUpForm.tsx
@@ -0,0 +1,33 @@
+import { Button, Paper, PasswordInput, TextInput, Title } from '@mantine/core'
+
+const SignUpForm = () => {
+  return (
+    <div>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900,
+        })}
+      >
+        Make an Account
+      </Title>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="user@email.com" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <TextInput label="Username" placeholder="Username" required mt="md" />
+        <Button fullWidth mt="xl">
+          Sign Up
+        </Button>
+      </Paper>
+    </div>
+  )
+}
+
+export default SignUpForm
