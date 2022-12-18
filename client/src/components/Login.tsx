bd28d25c55c60f88793b48a634afaa5855a22a87
Timothy <TomeSprout@pm.me>
2022-12-18 08:52:51 -0500

    Separate SignUp and Login to new components
    Import component into Grid
    Set media query for larger screen format
    Add divider on larger screens
    Set up dummy fetch request for auth

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,58 +1,47 @@
-import {
-  TextInput,
-  PasswordInput,
-  Checkbox,
-  Anchor,
-  Paper,
-  Title,
-  Text,
-  Container,
-  Group,
-  Button,
-} from '@mantine/core';
+import { Container, Divider, Grid } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
+import { useState } from 'react'
+import SignUpForm from './SignUpForm'
+import LoginForm from './LoginForm'
 
-const AuthenticationTitle = () => {
+const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const largeScreen = useMediaQuery('(min-width: 960px)')
+
+  const loginRequest = async () => {
+    console.warn(email, password)
+    let auth = { email, password }
+    let request = fetch('http://localhost:3500/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(auth),
+    })
+  }
+
   return (
-    <Container size={420} my={40}>
-      <Title
-        align="center"
-        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-      >
-        Make an Account
-      </Title>
-
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="user@email.com" required />
-        <PasswordInput label="Password" placeholder="Your password" required mt="md" />
-        <TextInput label="Username" placeholder="Username" required mt="md" />
-        <Button fullWidth mt="xl">
-          Sign Up
-        </Button>
-      </Paper>
-
-
-      <Title
-        align="center"
-        sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
-      >
-        Welcome back!
-      </Title>
-      
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="user@email.com" required />
-        <PasswordInput label="Password" placeholder="Your password" required mt="md" />
-        <Group position="apart" mt="lg">
-          <Checkbox label="Remember me" sx={{ lineHeight: 1 }} />
-          <Anchor<'a'> onClick={(event) => event.preventDefault()} href="#" size="sm">
-            Forgot password?
-          </Anchor>
-        </Group>
-        <Button fullWidth mt="xl">
-          Log in
-        </Button>
-      </Paper>
+    <Container size={largeScreen ? 'md' : 'xs'} my={largeScreen ? 350 : 40}>
+      <Grid grow gutter={70}>
+        <Grid.Col span={largeScreen ? 5 : 7}>
+          <SignUpForm />
+        </Grid.Col>
+        {largeScreen ? (
+          <Divider variant="solid" orientation="vertical" size="lg" />
+        ) : null}
+        <Grid.Col span={largeScreen ? 5 : 7}>
+          <LoginForm
+            setEmail={setEmail}
+            setPassword={setPassword}
+            login={loginRequest}
+          />
+        </Grid.Col>
+      </Grid>
     </Container>
-  );
+  )
 }
 
-export default AuthenticationTitle
+export default Login
