e58046a944ab0021c7fde6b48d69b83a634da680
Timothy <TomeSprout@pm.me>
2022-12-21 13:41:04 -0500

    Update vite.config.ts
    Import env variable for PORT
    Install deps for @node/types

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+  const CLIENT_PORT = parseInt(env.VITE_CLIENT_PORT)
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      strictPort: true,
+      port: CLIENT_PORT,
     },
-    host: '0.0.0.0',
-    strictPort: true,
-    port: 80,
-  },
-})
+  })
+}
