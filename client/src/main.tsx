fb034fd007dfe9e41f0c66052e5caf3791f68f4c
Timothy <TomeSprout@pm.me>
2022-12-24 20:41:57 -0500

    Add AuthProvider to global context for app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<App />} />
+        <AuthProvider>
+          <Route path="/*" element={<App />} />
+        </AuthProvider>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
