e27aa6a2d536b9219e8c8b70138ec5164abc433f
Timothy <TomeSprout@pm.me>
2022-12-28 10:09:50 -0500

    Add JWT verification to Users get route

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,8 +1,19 @@
+import { config } from 'dotenv'
 import { Router } from 'express'
-import { getUser, createNewUser } from '../controllers/usersController'
+import { expressjwt } from 'express-jwt'
+import { Secret } from 'jsonwebtoken'
+
+import { createNewUser, getUser } from '../controllers/usersController'
+
+config()
+
+const jwtSecret = process.env.ACCESS_TOKEN_SECRET as Secret
 
 const router: Router = Router()
 
-router.route('/').get(getUser).post(createNewUser)
+router
+  .route('/users')
+  .get(expressjwt({ secret: jwtSecret, algorithms: ['HS256'] }), getUser)
+  .post(createNewUser)
 
-module.exports = router
+export { router }
