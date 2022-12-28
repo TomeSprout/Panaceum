f533ac1e378be83a9790e972e42e9fe58dc56f2b
Timothy <TomeSprout@pm.me>
2022-12-28 15:00:23 -0500

    Move JWT logic to User service
    Check password sent in req
    Assign tokens for user auth
    Return object with user details and tokens

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,4 +1,7 @@
+import bcrypt from 'bcrypt'
+import { Secret, sign } from 'jsonwebtoken'
 import { DocumentDefinition } from 'mongoose'
+
 import User, { UserSchema } from '../models/User.model'
 
 export const register = async (user: DocumentDefinition<UserSchema>) => {
@@ -15,6 +18,45 @@ export const login = async (user: DocumentDefinition<UserSchema>) => {
       email: user.email,
       password: user.password,
     })
+
+    if (!foundUser) {
+      throw new Error('Email is incorrect')
+    }
+
+    const passwordMatch = bcrypt.compareSync(user.password, foundUser.password)
+    if (passwordMatch) {
+      const accessToken = sign(
+        {
+          _id: foundUser._id?.toString(),
+          email: foundUser.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET as Secret,
+        {
+          expiresIn: '2h',
+        }
+      )
+
+      const refreshToken = sign(
+        {
+          email: foundUser.email,
+        },
+        process.env.REFRESH_TOKEN_SECRET as Secret,
+        {
+          expiresIn: `1d`,
+        }
+      )
+
+      return {
+        user: {
+          _id: user._id,
+          email: user.email,
+        },
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      }
+    } else {
+      throw new Error('Password is incorrect')
+    }
   } catch (error) {
     throw error
   }
