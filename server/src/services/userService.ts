f1693b6f41a6a5374065e832c85f8639c9abf428
Timothy <TomeSprout@pm.me>
2022-12-28 11:14:27 -0500

    Add register service for User

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,9 +1,20 @@
 import { DocumentDefinition } from 'mongoose'
 import User, { UserSchema } from '../models/User.model'
 
+export const register = async (user: DocumentDefinition<UserSchema>) => {
+  try {
+    User.create(user)
+  } catch (error) {
+    throw error
+  }
+}
+
 export const login = async (user: DocumentDefinition<UserSchema>) => {
   try {
-    const foundUser = await User.findOne({ email: user.email, password: user.password })
+    const foundUser = await User.findOne({
+      email: user.email,
+      password: user.password,
+    })
   } catch (error) {
     throw error
   }
