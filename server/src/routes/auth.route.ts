f8d41cda2e6554924a4ff792c502f86675056d52
Timothy <TomeSprout@pm.me>
2023-01-11 22:00:53 -0500

    Add route for logout

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 
-import { handleLogin, handleRegistration } from '../controllers/authController'
+import { handleLogin, handleLogout, handleRegistration } from '../controllers/authController'
 
 const router: Router = Router()
 
 router.route('/')
 router.post('/signup', handleRegistration)
 router.post('/login', handleLogin)
+router.get('/logout', handleLogout)
 
 export = router
