ebcadefdc3781d0b3c8a05469a0efc6d4d938400
Timothy <TomeSprout@pm.me>
2023-01-11 22:00:39 -0500

    Remove unused declarations

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,13 +1,6 @@
-import { config } from 'dotenv'
 import { Router } from 'express'
-import { expressjwt } from 'express-jwt'
-import { Secret } from 'jsonwebtoken'
 
 import { handleLogin, handleRegistration } from '../controllers/authController'
-
-config()
-
-const jwtSecret = process.env.ACCESS_TOKEN_SECRET as Secret
 
 const router: Router = Router()
 
