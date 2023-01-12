93a9de73795f35c3edc95027dfe67f517eea1a62
Timothy <TomeSprout@pm.me>
2023-01-11 22:02:32 -0500

    Create refresh route
    GET - new accessToken

---
+++ server/src/routes/refresh.route.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+
+import handleRefreshToken from '../controllers/refreshTokenController'
+
+const router: Router = Router()
+
+router.get(
+  '/',
+  handleRefreshToken
+)
+
+export = router
