6c030fadc3bb3751cd080474f1bb3ae92c9d6b1b
Timothy <TomeSprout@pm.me>
2023-01-12 22:08:17 -0500

    Remove jwt verify function
    Move jwt verification to express-jwt middleware on /refresh route

--- server/src/controllers/refreshTokenController.ts
+++ server/src/controllers/refreshTokenController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
+import { Secret, sign } from 'jsonwebtoken'
 
 import User from '../models/User.model'
-import { Secret, sign, verify } from 'jsonwebtoken'
 
 const handleRefreshToken = (req: Request, res: Response) => {
   const cookies = req.cookies
@@ -19,27 +19,18 @@ const handleRefreshToken = (req: Request, res: Response) => {
   if (!foundUser) {
     return res.sendStatus(403)
   }
-  verify(
-    refreshToken,
-    process.env.REFRESH_TOKEN_SECRET as Secret,
-    (err: any, decoded: any) => {
-      if (err) {
-        return res.sendStatus(403)
-      }
 
-      const accessToken = sign(
-        {
-          email: decoded.email
-        },
-        process.env.ACCESS_TOKEN_SECRET as Secret,
-        {
-          expiresIn: '5m'
-        }
-      )
-
-      res.json({ accessToken })
+  const accessToken = sign(
+    {
+      email: foundUser.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET as Secret,
+    {
+      expiresIn: '5m',
     }
   )
+
+  res.json({ accessToken })
 }
 
 export default handleRefreshToken
