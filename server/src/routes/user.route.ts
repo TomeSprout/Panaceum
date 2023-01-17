fff70f9d75623ea408e5eefb3f6bd5bc90e6e510
Timothy <TomeSprout@pm.me>
2023-01-17 17:41:31 -0500

    Add POST method for user secrets
    POST to /user/secrets
    Creates new secret API key and saves to user document

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -3,7 +3,7 @@ import { Router } from 'express'
 import { expressjwt } from 'express-jwt'
 import { Secret } from 'jsonwebtoken'
 
-import { getUser } from '../controllers/usersController'
+import { getUser, handleSecret } from '../controllers/usersController'
 
 config()
 
@@ -16,6 +16,11 @@ router.get(
   '/:id',
   expressjwt({ secret: jwtSecret, algorithms: ['HS256'] }),
   getUser
+)
+router.post(
+  '/secrets',
+  expressjwt({ secret: jwtSecret, algorithms: ['HS256'] }),
+  handleSecret
 )
 
 export = router
